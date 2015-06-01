5b89c28a118118981aa8fbe33c28f787fb662c8e
Yavor Georgiev <yavor.georgiev@hulu.com>
2015-05-31 19:33:32 -0700

    Adding header and logic to hide disabled page components when appropriate

--- js/fb.js
+++ js/fb.js
@@ -11,10 +11,12 @@ function statusChangeCallback(response) {
   } else if (response.status === 'not_authorized') {
     // The person is logged into Facebook, but not your app.
     console.log('Please log into this app.');
+    loginNeeded();
   } else {
     // The person is not logged into Facebook, so we're not sure if
     // they are logged into this app or not.
     console.log('Please log into Facebook.');
+    loginNeeded();
   }
 }
 
